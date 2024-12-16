394d05abd793af89d7ae31eb36add33959e2e507
amirn003 <amir.naar@gmail.com>
2024-12-16 15:17:43 +0100

    start adding follow action to button

--- network/urls.py
+++ network/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("new_post", views.new_post, name="new_post"),
     path("add_post", views.add_post, name="add_post"),
     path("profile/<str:username>", views.profile, name="profile"),
+    path("follow/<str:username>", views.follow, name="follow"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
--- network/views.py
+++ network/views.py
@@ -43,6 +43,17 @@ def profile(request, username):
         "current_user": current_user
     })
 
+def follow(request, username):
+    user = User.objects.get(username=username)
+    current_user = request.user
+    current_user_id = current_user.id
+    is_following = current_user.following.filter(id=current_user_id).exists()
+    if is_following:
+        current_user.following.remove(user)
+    else:
+        current_user.following.add(user)
+    return HttpResponseRedirect(reverse("profile", args=(username,)))
+
 
 def login_view(request):
     if request.method == "POST":
